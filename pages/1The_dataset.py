f3a1a2f9fdf2210560b537f64b68027113bfbed1
James Cuadra <79429518+AdmiralFirefox@users.noreply.github.com>
2024-03-20 19:08:41 +0800

    Changed reshape value

--- pages/1The_dataset.py
+++ pages/1The_dataset.py
@@ -60,7 +60,7 @@ def app():
     st.subheader('First 25 images in the MNIST dataset') 
 
     # Get the first 25 images and reshape them to 28x28 pixels
-    train_images = np.array(X_train).reshape(-1, 28, 28)
+    train_images = np.array(X_train).reshape(-1, 28)
     train_labels = np.array(y_train)
     images = train_images[:25]
     # Create a 5x5 grid of subplots
